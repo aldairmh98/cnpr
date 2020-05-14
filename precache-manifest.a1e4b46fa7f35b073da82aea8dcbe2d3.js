262373302f45e4731016679e51a28c31e0cb944f
aldairmh <aldairmh02@gmail.com>
2020-05-13 19:36:48 -0500

    2

---
+++ precache-manifest.a1e4b46fa7f35b073da82aea8dcbe2d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "096906badad7f1fd188c5f3f133a14ca",
+    "url": "/cnpr/index.html"
+  },
+  {
+    "revision": "f01b85e4eeba3405a6d8",
+    "url": "/cnpr/static/css/main.42d38f09.chunk.css"
+  },
+  {
+    "revision": "f3ce638ea959a2fc1030",
+    "url": "/cnpr/static/js/2.064db90b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cnpr/static/js/2.064db90b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f01b85e4eeba3405a6d8",
+    "url": "/cnpr/static/js/main.96eb76fd.chunk.js"
+  },
+  {
+    "revision": "fd51f88f9b745f47f804",
+    "url": "/cnpr/static/js/runtime-main.4d4cc76e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cnpr/static/media/logo.5d5d9eef.svg"
+  }
+]);
